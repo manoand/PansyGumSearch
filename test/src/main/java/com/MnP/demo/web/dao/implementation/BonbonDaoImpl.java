52b6cbffd0cff8477923ce205fa02fbc97491b16
manoand <manoa.andriamanampisoa@viveris.fr>
2018-01-29 15:56:21 +0100

    Nettoyage

--- test/src/main/java/com/MnP/demo/web/dao/implementation/BonbonDaoImpl.java
+++ test/src/main/java/com/MnP/demo/web/dao/implementation/BonbonDaoImpl.java
@@ -1,93 +1,159 @@
 package com.MnP.demo.web.dao.implementation;
 
 import java.io.IOException;
-import java.util.Collections;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.apache.http.Header;
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
-import org.apache.http.ParseException;
-import org.apache.http.entity.ContentType;
-import org.apache.http.message.BasicHeader;
-import org.apache.http.nio.entity.NStringEntity;
-import org.apache.http.util.EntityUtils;
-import org.elasticsearch.client.Response;
+import org.apache.log4j.Logger;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
 import org.springframework.stereotype.Service;
 
 import com.MnP.demo.web.dao.BonbonDao;
 import com.MnP.demo.web.model.Bonbon;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class BonbonDaoImpl implements BonbonDao {
 
-    
+    private static Logger LOGGER = Logger.getLogger(BonbonDaoImpl.class);
+
     @Override
     public void delete(final Bonbon bonbon) {
-    	
+
     }
 
     @Override
-    public Iterable<Bonbon> findAll() {
-    	
-		return null;
+    public List<Bonbon> findAll() {
+        final HttpHost tab[] = { new HttpHost("localhost", 9200, "http"), new HttpHost("localhost", 9201, "http") };
+        final RestHighLevelClient client = new RestHighLevelClient(RestClient.builder(tab));
+        final SearchRequest searchRequest = new SearchRequest("table");
+        final ObjectMapper mapper = new ObjectMapper();
+
+        SearchHits searchHits = null;
+        final List<Bonbon> lstBonbon = new ArrayList<Bonbon>();
+        searchRequest.types("bonbon");
+        SearchResponse searchResponse = null;
+        try {
+            searchResponse = client.search(searchRequest);
+        } catch (final IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            try {
+                client.close();
+            } catch (final IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        if (searchResponse != null) {
+            searchHits = searchResponse.getHits();
+            for (final SearchHit hit : searchHits) {
+                try {
+                    lstBonbon.add(mapper.readValue(hit.getSourceAsString(), Bonbon.class));
+                } catch (JsonParseException | JsonMappingException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (final IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+        return lstBonbon;
     }
 
     @Override
     public Bonbon findById(final Long id) {
-    	
-		return null;
+        final HttpHost tab[] = { new HttpHost("localhost", 9200, "http"), new HttpHost("localhost", 9201, "http") };
+        final RestHighLevelClient client = new RestHighLevelClient(RestClient.builder(tab));
+        final ObjectMapper mapper = new ObjectMapper();
+
+        Bonbon res = null;
+        GetResponse responseGet = null;
+        final GetRequest getRequest = new GetRequest("table", "bonbon", id.toString());
+        try {
+            responseGet = client.get(getRequest);
+        } catch (final IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        try {
+            if (responseGet.isExists()) {
+                res = mapper.readValue(responseGet.getSourceAsString(), Bonbon.class);
+            }
+        } catch (JsonParseException | JsonMappingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (final IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            try {
+                client.close();
+            } catch (final IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        return res;
     }
+
+    // @Autowired
+    // public void setBookRepository(final BonbonRepository bonbonRepository) {
+    // close();
+    // }
 
     @Override
-    public Bonbon save(final Bonbon bonbon) {
-    	RestClientBuilder builder = RestClient.builder(new HttpHost("localhost", 9200, "http"));
-    	RestClient client = builder.build();
-    	Response response = null;
-    	Map<String, String> params = Collections.emptyMap();
-    	String jsonString = "{" +
-    	            "\"id\":\""+bonbon.getId() +"\"," +
-    	            "\"nom\":\""+bonbon.getNom() +"\"," +
-    	            "\"couleur\":\""+bonbon.getCouleur() +"\"," +
-    	            "\"poids\":\""+bonbon.getPoids() +"\"," +
-    	            "\"composition\":\""+bonbon.getComposition() +"\""+
-    	        "}";
-    	HttpEntity entity = new NStringEntity(jsonString, ContentType.APPLICATION_JSON);
-    	try {
-    		response = client.performRequest("PUT", "/table/bonbon/1", params, entity);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	ObjectMapper mapper = new ObjectMapper();
-    	
-    	Bonbon res = new Bonbon();
-    	try {
-			res = mapper.readValue(EntityUtils.toString(response.getEntity()), Bonbon.class);
-		} catch (ParseException | IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	close(client);
-		return res;
+    public void save(final Bonbon bonbon) {
+        final HttpHost tab[] = { new HttpHost("localhost", 9200, "http"), new HttpHost("localhost", 9201, "http") };
+        final RestHighLevelClient client = new RestHighLevelClient(RestClient.builder(tab));
+        final String jsonString =
+                "{"
+                        + "\"id\":\""
+                        + bonbon.getId()
+                        + "\","
+                        + "\"nom\":\""
+                        + bonbon.getNom()
+                        + "\","
+                        + "\"couleur\":\""
+                        + bonbon.getCouleur()
+                        + "\","
+                        + "\"poids\":\""
+                        + bonbon.getPoids()
+                        + "\","
+                        + "\"composition\":\""
+                        + bonbon.getComposition()
+                        + "\""
+                        + "}";
+
+        final IndexRequest request = new IndexRequest("table", "bonbon", bonbon.getId().toString());
+        request.source(jsonString, XContentType.JSON);
+        try {
+            client.index(request);
+
+        } catch (final IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            try {
+                client.close();
+            } catch (final IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
     }
-
-//    @Autowired
-//    public void setBookRepository(final BonbonRepository bonbonRepository) {
-//    	close();
-//    }
-    
-    private void close( RestClient client) {
-    	try {
-			client.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    }
-
-
 }
--- test/src/test/java/com/MnP/demo/web/dao/test/BonbonDaoTest.java
+++ test/src/test/java/com/MnP/demo/web/dao/test/BonbonDaoTest.java
@@ -2,14 +2,11 @@ package com.MnP.demo.web.dao.test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.MnP.demo.web.DemoApplication;
@@ -22,48 +19,12 @@ public class BonbonDaoTest {
     @Autowired
     private BonbonDao bonbonDao;
 
-//    @Autowired
-//    private ElasticsearchTemplate esTemplate;
-//
-//    @Before
-//    public void before() {
-//        esTemplate.deleteIndex(Bonbon.class);
-//        esTemplate.createIndex(Bonbon.class);
-//        esTemplate.putMapping(Bonbon.class);
-//        esTemplate.refresh(Bonbon.class);
-//    }
-//
-//    @Test
-//    public void testDelete() {
-//
-//        final Bonbon bonbon = new Bonbon(Long.valueOf("1234"), "bonbonTest", "rouge", Double.valueOf("1.2"), "sucre,vanille,amande");
-//        bonbonDao.save(bonbon);
-//        bonbonDao.delete(bonbon);
-//        final Bonbon testBonbon = bonbonDao.findById(bonbon.getId());
-//        assertNull(testBonbon);
-//    }
-//
-//    @Test
-//    public void testFindById() {
-//
-//        final Bonbon bonbon = new Bonbon(Long.valueOf("1234"), "bonbonTest", "rouge", Double.valueOf("1.2"), "sucre,vanille,amande");
-//        bonbonDao.save(bonbon);
-//
-//        final Bonbon testBonbon = bonbonDao.findById(bonbon.getId());
-//        assertNotNull(testBonbon.getId());
-//        assertEquals(testBonbon.getId(), bonbon.getId());
-//        assertEquals(testBonbon.getComposition(), bonbon.getComposition());
-//        assertEquals(testBonbon.getNom(), bonbon.getNom());
-//        assertEquals(testBonbon.getPoids(), bonbon.getPoids());
-//        assertEquals(testBonbon.getCouleur(), bonbon.getCouleur());
-//    }
-
     @Test
     public void testSave() {
 
         final Bonbon bonbon = new Bonbon(Long.valueOf("1234"), "bonbonTest", "rouge", Double.valueOf("1.2"), "sucre,vanille,amande");
-        final Bonbon testBonbon = bonbonDao.save(bonbon);
-
+        bonbonDao.save(bonbon);
+        final Bonbon testBonbon = bonbonDao.findById(bonbon.getId());
         assertNotNull(testBonbon.getId());
         assertEquals(testBonbon.getId(), bonbon.getId());
         assertEquals(testBonbon.getComposition(), bonbon.getComposition());
